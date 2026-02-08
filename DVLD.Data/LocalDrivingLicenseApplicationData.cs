319088b56f3e8fd10f7ea5c8dfb191524db87e8b
Mohamed Ragheb <mohamedraghebomer@gmail.com>
2026-02-08 11:21:46 +0200

    Create DAL for Applications and LocalDrivingLicenseApplications fixes #22 fixes #23

---
+++ DVLD.Data/LocalDrivingLicenseApplicationData.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using DVLD.Core.DTOs.Entities;
+using DVLD.Core.DTOs.Enums;
+using DVLD.Core.Logging;
+using DVLD.Data.Settings;
+
+namespace DVLD.Data
+{
+    public static class LocalDrivingLicenseApplicationData
+    {
+        public static int Add(LocalDrivingLicenseApplication localDrivingLicenseApplication)
+        {
+            string query = @"INSERT INTO LocalDrivingLicenseApplications (ApplicationId, LicenseClassId)
+                            VALUES (@ApplicationId, @LicenseClassId);
+                            SELECT SCOPE_IDENTITY();";
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(DataSettings.connectionString))
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@ApplicationId", localDrivingLicenseApplication.ApplicationId);
+                    command.Parameters.AddWithValue("@LicenseClassId", (int)localDrivingLicenseApplication.LicenseClassId);
+                    connection.Open();
+                    object result = command.ExecuteScalar();
+                    if (result != null && int.TryParse(result.ToString(), out int newLocalDrivingLicenseApplicationId))
+                        return newLocalDrivingLicenseApplicationId;
+                }
+                return -1; // Indicate failure to insert
+            }
+            catch (Exception ex)
+            {
+                AppLogger.LogError("DAL: Error while inserting into LocalDrivingLicenseApplications table.", ex);
+                throw;
+            }
+        }
+
+        public static DataTable GetAll()
+        {
+            string query = @"SELECT * FROM LocalDrivingLicenseApplications";
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(DataSettings.connectionString))
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        DataTable dataTable = new DataTable();
+                        dataTable.Load(reader);
+                        return dataTable;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                AppLogger.LogError("DAL: Error while retrieving data from LocalDrivingLicenseApplications table.", ex);
+                throw;
+            }
+        }
+
+        public static LocalDrivingLicenseApplication GetById(int localDrivingLicenseApplicationId)
+        {
+            string query = @"SELECT * FROM LocalDrivingLicenseApplications WHERE LocalDrivingLicenseApplicationId = @LocalDrivingLicenseApplicationId";
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(DataSettings.connectionString))
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@LocalDrivingLicenseApplicationId", localDrivingLicenseApplicationId);
+                    connection.Open();
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            return new LocalDrivingLicenseApplication
+                            (
+                                localDrivingLicenseApplicationId: Convert.ToInt32(reader["LocalDrivingLicenseApplicationId"]),
+                                applicationId: Convert.ToInt32(reader["ApplicationId"]),
+                                licenseClassId: (LicenseClass)Convert.ToInt32(reader["LicenseClassId"])
+                            );
+                        }
+                    }
+                }
+                return null; // Not found
+            }
+            catch (Exception ex)
+            {
+                AppLogger.LogError("DAL: Error while retrieving a specific record from LocalDrivingLicenseApplications table.", ex);
+                throw;
+            }
+        }
+
+        public static LocalDrivingLicenseApplication GetByApplicationId(int applicationId)
+        {
+            string query = @"SELECT * FROM LocalDrivingLicenseApplications WHERE ApplicationId = @ApplicationId";
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(DataSettings.connectionString))
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@ApplicationId", applicationId);
+                    connection.Open();
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            return new LocalDrivingLicenseApplication
+                            (
+                                localDrivingLicenseApplicationId: Convert.ToInt32(reader["LocalDrivingLicenseApplicationId"]),
+                                applicationId: Convert.ToInt32(reader["ApplicationId"]),
+                                licenseClassId: (LicenseClass)Convert.ToInt32(reader["LicenseClassId"])
+                            );
+                        }
+                    }
+                }
+                return null; // Not found
+            }
+            catch (Exception ex)
+            {
+                AppLogger.LogError("DAL: Error while retrieving a specific record from LocalDrivingLicenseApplications table by ApplicationId.", ex);
+                throw;
+            }
+        }
+
+
+    }
+}
