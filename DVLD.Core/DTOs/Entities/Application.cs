613e8f9408415c8cdcdaf834f02372cd4e1e101f
Mohamed Ragheb <mohamedraghebomer@gmail.com>
2026-02-08 10:42:40 +0200

    Create DTOs for Applications and LocalDrivingLicenseApplications fixex #21

---
+++ DVLD.Core/DTOs/Entities/Application.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using DVLD.Core.DTOs.Enums;
+using DVLD.Core.Exceptions;
+
+namespace DVLD.Core.DTOs.Entities
+{
+    public class Application
+    {
+        private int _applicationId;
+        private int _applicantPersonId;
+        private DateTime _applicationDate;
+        private ApplicationType _applicationTypeId;
+        private ApplicationStatus _applicationStatus;
+        private DateTime _lastStatusDate;
+        private decimal _paidFees;
+        private int _createdByUserId;
+
+
+        public int ApplicationId
+        {
+            get => _applicationId;
+            private set
+            {
+                if (value <= 0)
+                    throw new ValidationException("ApplicationId must be a positive integer.");
+                _applicationId = value;
+            }
+        }
+
+        public int ApplicantPersonId
+        {
+            get => _applicantPersonId;
+            set
+            {
+                if (value <= 0)
+                    throw new ValidationException("ApplicantPersonId must be a positive integer.");
+                _applicantPersonId = value;
+            }
+        }
+
+        public DateTime ApplicationDate
+        {
+            get => _applicationDate;
+            set
+            {
+                if (value > DateTime.Now)
+                    throw new ValidationException("ApplicationDate cannot be in the future.");
+                _applicationDate = value;
+            }
+        }
+
+        public ApplicationType ApplicationTypeId
+        {
+            get => _applicationTypeId;
+            set
+            {
+                if (!Enum.IsDefined(typeof(ApplicationType), value))
+                    throw new ValidationException("Invalid ApplicationTypeId.");
+                _applicationTypeId = value;
+            }
+        }
+
+        public ApplicationStatus ApplicationStatus
+        {
+            get => _applicationStatus;
+            set
+            {
+                if (!Enum.IsDefined(typeof(ApplicationStatus), value))
+                    throw new ValidationException("Invalid ApplicationStatus.");
+                _applicationStatus = value;
+            }
+        }
+
+        public DateTime LastStatusDate
+        {
+            get => _lastStatusDate;
+            set
+            {
+                if (value > DateTime.Now)
+                    throw new ValidationException("LastStatusDate cannot be in the future.");
+                _lastStatusDate = value;
+            }
+        }
+
+        public decimal PaidFees
+        {
+            get => _paidFees;
+            set
+            {
+                if (value < 0)
+                    throw new ValidationException("PaidFees cannot be negative.");
+                _paidFees = value;
+            }
+        }
+
+        public int CreatedByUserId
+        {
+            get => _createdByUserId;
+            set
+            {
+                if (value <= 0)
+                    throw new ValidationException("CreatedByUserId must be a positive integer.");
+                _createdByUserId = value;
+            }
+        }
+
+
+        public Application()
+        {
+            this._applicationId = -1; // Default value indicating not set
+            this._applicantPersonId = -1; // Default value indicating not set
+            this._applicationDate = new DateTime(1, 1, 1); // Default value indicating not set
+            this._applicationTypeId = 0; // Default value indicating not set
+            this._applicationStatus = 0; // Default value indicating not set
+            this._lastStatusDate = new DateTime(1, 1, 1); // Default value indicating not set
+            this._paidFees = 0; // Default value indicating not set
+            this._createdByUserId = -1; // Default value indicating not set
+        }
+
+        public Application(int applicantPersonId, DateTime applicationDate, ApplicationType applicationTypeId, ApplicationStatus applicationStatus, DateTime lastStatusDate, decimal paidFees, int createdByUserId) : this()
+        {
+            ApplicantPersonId = applicantPersonId;
+            ApplicationDate = applicationDate;
+            ApplicationTypeId = applicationTypeId;
+            ApplicationStatus = applicationStatus;
+            LastStatusDate = lastStatusDate;
+            PaidFees = paidFees;
+            CreatedByUserId = createdByUserId;
+        }
+
+        internal Application(int applicationId, int applicantPersonId, DateTime applicationDate, ApplicationType applicationTypeId, ApplicationStatus applicationStatus, DateTime lastStatusDate, decimal paidFees, int createdByUserId) : this()
+        {
+            ApplicationId = applicationId;
+            ApplicantPersonId = applicantPersonId;
+            ApplicationDate = applicationDate;
+            ApplicationTypeId = applicationTypeId;
+            ApplicationStatus = applicationStatus;
+            LastStatusDate = lastStatusDate;
+            PaidFees = paidFees;
+            CreatedByUserId = createdByUserId;
+        }
+    }
+}
---
+++ DVLD.Core/DTOs/Entities/LocalDrivingLicenseApplication.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using DVLD.Core.DTOs.Enums;
+using DVLD.Core.Exceptions;
+
+namespace DVLD.Core.DTOs.Entities
+{
+    public class LocalDrivingLicenseApplication
+    {
+        private int _localDrivingLicenseApplicationId;
+        private int _applicationId;
+        private LicenseClass _licenseClassId;
+
+        public int LocalDrivingLicenseApplicationId
+        {
+            get => _localDrivingLicenseApplicationId;
+            private set
+            {
+                if (value <= 0)
+                    throw new ValidationException("LocalDrivingLicenseApplicationId must be a positive integer.");
+                _localDrivingLicenseApplicationId = value;
+            }
+        }
+
+        public int ApplicationId
+        {
+            get => _applicationId;
+            set
+            {
+                if (value <= 0)
+                    throw new ValidationException("ApplicationId must be a positive integer.");
+                _applicationId = value;
+            }
+        }
+
+        public LicenseClass LicenseClassId
+        {
+            get => _licenseClassId;
+            set
+            {
+                if (!Enum.IsDefined(typeof(LicenseClass), value))
+                    throw new ValidationException("Invalid LicenseClassId.");
+                _licenseClassId = value;
+            }
+        }
+
+
+        public LocalDrivingLicenseApplication()
+        {
+            this._localDrivingLicenseApplicationId = -1;
+            this._applicationId = -1;
+            this._licenseClassId = 0; // Default value indicating not set
+        }
+
+        public LocalDrivingLicenseApplication(int applicationId, LicenseClass licenseClassId) : this()
+        {
+            ApplicationId = applicationId;
+            LicenseClassId = licenseClassId;
+        }
+
+        internal LocalDrivingLicenseApplication(int localDrivingLicenseApplicationId, int applicationId, LicenseClass licenseClassId) : this()
+        {
+            LocalDrivingLicenseApplicationId = localDrivingLicenseApplicationId;
+            ApplicationId = applicationId;
+            LicenseClassId = licenseClassId;
+        }
+    }
+}
