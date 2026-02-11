de750597678670983e6900603665e45593c6bd59
Mohamed Ragheb <mohamedraghebomer@gmail.com>
2026-02-11 17:53:17 +0200

    feat: Create DTO for Test fixes #45

---
+++ DVLD.Core/DTOs/Entities/Test.cs
@@ -0,0 +1,88 @@
+﻿using DVLD.Core.Exceptions;
+
+namespace DVLD.Core.DTOs.Entities
+{
+    public class Test
+    {
+        private int _testId;
+        private int _testAppointmentId;
+        private bool _testResult;
+        private string _notes;
+        private int _createdByUserId;
+
+        
+        public int TestID
+        {
+            get => _testId;
+
+            private set
+            {
+                if (value <= 0)
+                    throw new ValidationException("Test Id must be a positive integer.");
+                _testId = value;
+            }
+        }
+
+        public int TestAppointmentID
+        {
+            get => _testAppointmentId;
+
+            set
+            {
+                if (value <= 0)
+                    throw new ValidationException("Tets Appointment Id must be a positive intger.");
+                _testAppointmentId = value;
+            }
+        }
+
+        public bool TestResult
+        {
+            get => _testResult;
+            set => _testResult = value;
+        }
+
+        public string Notes
+        {
+            get => _notes;
+            set => _notes = value;
+        }
+
+        public int CreatedByUserID
+        {
+            get => _createdByUserId;
+
+            set
+            {
+                if (value <= 0)
+                    throw new ValidationException("User Id must be a positve intger.");
+            }
+        }
+
+
+        public Test()
+        {
+            this._testId = -1;
+            this._testAppointmentId = -1;
+            this._testResult = false;
+            this._notes = string.Empty;
+            this._createdByUserId = -1;
+        }
+
+        public Test(int testAppointmentId, bool testResult, string notes, int createdByUserId) : this()
+        {
+            TestAppointmentID = testAppointmentId;
+            TestResult = testResult;
+            Notes = notes;
+            CreatedByUserID = createdByUserId;
+        }
+
+        internal Test(int testId, int testAppointmentId, bool testResult, string notes, int createdByUserId) : this()
+        {
+            TestID = testId;
+            TestAppointmentID = testAppointmentId;
+            TestResult = testResult;
+            Notes = notes;
+            CreatedByUserID = createdByUserId;
+        }
+    }
+}
