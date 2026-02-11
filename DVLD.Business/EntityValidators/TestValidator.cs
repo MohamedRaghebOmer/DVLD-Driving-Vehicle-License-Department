8b7c46f8ceb06f60c930293901c47489241ea424
Mohamed Ragheb <mohamedraghebomer@gmail.com>
2026-02-11 18:37:19 +0200

    feat: Create Core & EntityValidators to Tests fixes #37

---
+++ DVLD.Business/EntityValidators/TestValidator.cs
@@ -0,0 +1,23 @@
+﻿using DVLD.Core.DTOs.Entities;
+using DVLD.Core.Exceptions;
+using DVLD.Data;
+
+namespace DVLD.Business.EntityValidators
+{
+    internal class TestValidator
+    {
+        public static void AddNewValidator(Test test)
+        {
+            Core.Validators.TestValidator.Validate(test);
+
+            if (!TestAppointmentData.Exists(test.TestAppointmentID))
+                throw new ValidationException("The specified TestAppointmentID does not exist.");
+
+            if (TestData.DoesTestAppointmentExists(test.TestAppointmentID))
+                throw new ValidationException("A test with the same TestAppointmentID already exists.");
+
+            if (!UserData.Exists(test.CreatedByUserID))
+                throw new ValidationException("The specified CreatedByUserID does not exist.");
+        }
+    }
+}
